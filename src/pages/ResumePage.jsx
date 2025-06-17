c35e66fb36851f638216ca0f4232a4e0d9fc81c4
MitchSouthwick <southwickmitch@gmail.com>
2025-06-17 12:23:46 -0600

    Added Updated resume and kept old one on file.

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -2,7 +2,7 @@ const ResumePage = () => (
     <div className="p-8 text-white">
       <h2 className="text-2xl font-bold mb-4">My Resume</h2>
       <iframe
-        src="/MitchSouthwickResume-2025.pdf"
+        src="/MitchSouthwickResume-2025_Updated.pdf"
         title="Resume"
         className="w-full h-[80vh] border"
       />
